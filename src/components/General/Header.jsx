c03a8a6818d851aa2804ecc2d1220295d5286c87
Jemzkie <jemrowynn506@gmail.com>
2025-05-26 14:51:41 +0800

    tab logo change and small fixes

--- src/components/General/Header.jsx
+++ src/components/General/Header.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 function Header({ ViewData }) {
   return (
     <>
-      <header className="w-full h-auto font-roboto md:flex flex-row justify-center items-center hidden">
-        <div className="logo flex justify-center items-center">
-          <img src="/logo.png" alt="Availio Logo" className="h-10" />
+      <header className="w-full h-auto font-roboto md:flex flex-row justify-between hidden">
+        <div className="logo">
+        <img src="/logo.png" alt="Availio Logo" className="h-15" />
         </div>
         <nav className="flex gap-5 text-2xl text-[#2E709E] items-center">
           <Link
@@ -19,9 +19,6 @@ function Header({ ViewData }) {
           >
             Homepage
           </Link>
-          <Link className="hover:text-black" to="/about">
-            About Us
-          </Link>
           <Link
             className="bg-[#E60000] rounded-lg text-white px-4 py-2 cursor-pointer"
             to="/login"
@@ -31,8 +28,8 @@ function Header({ ViewData }) {
         </nav>
       </header>
       <header className="md:hidden flex w-full h-auto flex-col gap-2">
-        <div className="logo flex justify-center items-center w-full">
-          <img src="/logo.png" alt="Availio Logo" className="h-10" />
+        <div className="logo">
+        <img src="/logo.png" alt="Availio Logo" className="h-15" />
         </div>
         <nav className=""></nav>
       </header>
